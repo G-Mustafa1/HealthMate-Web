ee47822346af0d467686f3f8270231af529e1ea4
Ghulam Mustafa <gmustufa1255@gmail.com>
2026-03-06 16:57:56 +0500

    update

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-40">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="w-10 h-10 border-4 border-gray-300 border-t-[#1d9f9c] rounded-full animate-spin"></div>
     </div>
   );
