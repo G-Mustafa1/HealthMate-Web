ddea6519384736360fca17568e196efae533603e
Ghulam Mustafa <gmustufa1255@gmail.com>
2026-03-06 16:40:12 +0500

    Adding Loader

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-6 h-6 border-4 border-[#1d9f9c]/30 border-t-[#1d9f9c] rounded-full animate-spin"></div>
+    <div className="flex justify-center items-center h-40">
+      <div className="w-10 h-10 border-4 border-gray-300 border-t-[#1d9f9c] rounded-full animate-spin"></div>
     </div>
   );
 };
